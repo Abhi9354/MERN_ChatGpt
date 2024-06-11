bf75e7d10003ade96b217f70c83f2c74b2d7a6e7
knowledge <abhi7ty4@gmail.com>
2024-06-11 15:13:40 +0530

    changes in origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,18 +9,18 @@ import { fileURLToPath } from "url"
 config();
 const app= express();
 //middlewares
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:"http://localhost:5174",credentials:true}))
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET))
 //remove it in prodection
 app.use(morgan("dev"))
 
 app.use("/api/v1",appRouter)
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-console.log('dirname',__dirname);
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
+// console.log('dirname',__dirname);
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')))
+// app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
 
 app.get('*', (req, res) => {
